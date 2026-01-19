4b59b25d332770ffc748927f1c6de0767d3747a3
abdaalsy <abdaalsy@gmail.com>
2026-01-19 16:55:29 -0500

    separate deciding next state and updating outputs

--- src/spi_peripheral.v
+++ src/spi_peripheral.v
@@ -11,11 +11,11 @@ module spi_peripheral (
     input   wire    nCS,
     input   wire    SCLK,
     input   wire    COPI,
-    output  reg [7:0] en_reg_out_7_0 = 8'h00,
-    output  reg [7:0] en_reg_out_15_8 = 8'h00,
-    output  reg [7:0] en_reg_pwm_7_0 = 8'h00,
-    output  reg [7:0] en_reg_pwm_15_8 = 8'h00,
-    output  reg [7:0] pwm_duty_cycle = 8'h00
+    output  reg [7:0] en_reg_out_7_0,
+    output  reg [7:0] en_reg_out_15_8,
+    output  reg [7:0] en_reg_pwm_7_0,
+    output  reg [7:0] en_reg_pwm_15_8,
+    output  reg [7:0] pwm_duty_cycle
 );
     // states for peripheral
     typedef enum logic [1:0] {
@@ -24,25 +24,16 @@ module spi_peripheral (
         RECV,
         FINISH
     } state_t;
-    state_t current_state = RESET;
-
+    state_t current_state, next_state;
+    
+    // registers for deciding output
+    reg [5:0] bit_count;
     reg [6:0] address;
     reg [7:0] value;
     reg transaction_ready;
-
     reg[2:0] sclk_sreg;         // shift reg for SCLK
     reg[2:0] ncs_sreg;          // shift reg for nCS
     reg[15:0] copi_sreg;        // shift reg for COPI
-
-    always @(posedge clk or negedge rst_n) begin
-        // check if we're in the reset state
-        if (!rst_n) begin 
-            current_state <= RESET;
-        end
-        // shift our SCLK and nCS registers
-        sclk_sreg <= {sclk_sreg[1:0], SCLK};
-        ncs_sreg <= {ncs_sreg[1:0], nCS};
-    end
 
     // for both registers, the newest flip-flop was just sampled and thus needs time to settle
     // so we're gonna perform our condition on the 2 stable older values
@@ -50,63 +41,88 @@ module spi_peripheral (
     wire ncs_negedge = (ncs_sreg[1] == 1'b0 && ncs_sreg[2] == 1'b1);
     wire ncs_posedge = (ncs_sreg[1] == 1'b1 && ncs_sreg[2] == 1'b0);
     
-    // this is where our state logic is gonna happen
-    reg [3:0] bit_count;
-    always @(*) begin
-        if (ncs_posedge) begin
-            current_state = FINISH;
+    always @(posedge clk or negedge rst_n) begin
+        // check if we're in the reset state
+        if (!rst_n) begin
+            current_state <= RESET;
         end
+        // shift our SCLK and nCS registers
+        current_state <= next_state;
+        sclk_sreg <= {sclk_sreg[1:0], SCLK};
+        ncs_sreg <= {ncs_sreg[1:0], nCS};
+        
         case (current_state)
             RESET: begin 
-                en_reg_out_7_0 = 8'h00;
-                en_reg_out_15_8 = 8'h00;
-                en_reg_pwm_7_0 = 8'h00;
-                en_reg_pwm_15_8 = 8'h00;
-                pwm_duty_cycle = 8'h00;
-                address = 7'b0000000;
-                value = 8'h00;
-                transaction_ready = 1'b0;
-                current_state = IDLE;
+                address <= 7'b0000000;
+                value <= 8'h00;
+                transaction_ready <= 1'b0;
+                bit_count <= 5'h00;
+                sclk_sreg <= 3'b111;
+                ncs_sreg <= 3'b111;
+                copi_sreg <= 16'h0000;
+                en_reg_out_7_0 <= 8'h00;
+                en_reg_out_15_8 <= 8'h00;
+                en_reg_pwm_7_0 <= 8'h00;
+                en_reg_pwm_15_8 <= 8'h00;
+                pwm_duty_cycle <= 8'h00;
             end
-            IDLE: begin
-                address = copi_sreg[14:8];
-                value = copi_sreg[7:0];
-                transaction_ready = 1'b1;
-                bit_count = 4'b0000;
-                if (ncs_negedge) begin
-                    current_state = RECV;
-                end
+            IDLE: begin 
+                address <= 7'b0000000;
+                value <= 8'h00;
+                transaction_ready <= 1'b1;
+                bit_count <= 5'h00;
+                copi_sreg <= 16'h0000;
+                en_reg_out_7_0 <= 8'h00;
+                en_reg_out_15_8 <= 8'h00;
+                en_reg_pwm_7_0 <= 8'h00;
+                en_reg_pwm_15_8 <= 8'h00;
+                pwm_duty_cycle <= 8'h00;
             end
-            RECV: begin
-                transaction_ready = 1'b0;
-                address = 7'b0000000;
-                value = 8'h00;
+            RECV: begin 
+                address <= 7'b0000000;
+                value <= 8'h00;
+                transaction_ready <= 1'b0;
                 if (sclk_posedge) begin
-                    copi_sreg = {copi_sreg[14:0], COPI};
-                    bit_count = bit_count + 1'b1;
-                    if (bit_count == 4'b0000) begin
-                        current_state = FINISH;
-                    end else begin
-                        current_state = RECV;
-                    end
-                end else begin
-                    current_state = RECV;
+                    copi_sreg <= {copi_sreg[14:0], COPI};
+                    bit_count <= bit_count + 1'b1;
                 end
+                en_reg_out_7_0 <= 8'h00;
+                en_reg_out_15_8 <= 8'h00;
+                en_reg_pwm_7_0 <= 8'h00;
+                en_reg_pwm_15_8 <= 8'h00;
+                pwm_duty_cycle <= 8'h00;
             end
-            FINISH: begin
-                address = copi_sreg[14:8];
-                value = copi_sreg[7:0];
-                transaction_ready = 1'b1;
-                case (address) 
-                    7'b0000000: en_reg_out_7_0 = value;   // address 0x00
-                    7'b0000001: en_reg_out_15_8 = value;  // address 0x01
-                    7'b0000010: en_reg_pwm_7_0 = value;   // address 0x02
-                    7'b0000011: en_reg_pwm_15_8 = value;  // address 0x03
-                    7'b0000100: pwm_duty_cycle = value;   // address 0x04
-                    default: current_state = IDLE;              // this will catch out of bounds address
+            FINISH: begin 
+                address <= copi_sreg[14:8];
+                value <= copi_sreg[7:0];
+                transaction_ready <= 1'b0;
+                copi_sreg <= copi_sreg;
+                bit_count <= 5'h00;
+                case (address)
+                    7'b0000000: en_reg_out_7_0 <= value;
+                    7'b0000001: en_reg_out_15_8 <= value;
+                    7'b0000010: en_reg_pwm_7_0 <= value;
+                    7'b0000011: en_reg_pwm_15_8 <= value;
+                    7'b0000100: pwm_duty_cycle <= value;
+                    default: ;
                 endcase
-                current_state = IDLE;
             end
         endcase
+    end
+    
+    // combinational logic to determine next state
+    always @(*) begin
+        next_state = current_state;
+        if (ncs_posedge) begin 
+            next_state = FINISH;
+        end else begin 
+            case (current_state)
+                RESET: next_state = IDLE;
+                IDLE: if (ncs_negedge) next_state = RECV;
+                RECV: if (bit_count = 5'b10000) next_state = FINISH;
+                FINISH: next_state = IDLE;
+            endcase
+            if (ncs_negedge) next_state = IDLE;
+        end
     end
 endmodule
